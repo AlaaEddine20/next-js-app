9308f6f998513cc1add0dae5aa681f9a7d264c6d
Alaa Eddine El Moudden <alaaeddine@MacBook-Air-di-Alaa.local>
2023-07-25 12:03:19 +0200

    adds getStaticPaths for pre-loading ids/params

--- pages/[id].js
+++ pages/[id].js
@@ -2,16 +2,21 @@ import React from "react";
 import fs from "fs";
 import path from "path";
 
+const getData = () => {
+  const filePath = path.join(process.cwd(), "data", "data.json");
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+};
+
 export async function getStaticProps(ctx) {
   // NextJs best practice: get id from params on server to get the advantage of pre-rendering
   // the id is extracted before the page renders, content is then ready for rendering
   const { params } = ctx;
-
   const productId = params.id;
 
-  const filePath = path.join(process.cwd(), "data", "data.json");
-  const jsonData = fs.readFileSync(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   return {
     props: {
@@ -20,8 +25,26 @@ export async function getStaticProps(ctx) {
   };
 }
 
+// Next js doesn't know which ids to pre-render, it needs getStaticPaths function to run after getStaticProps
+// we provide the function the ids with the fallback which allows to pre-render only some ids/params when set to true
+// in this case is set to false because we need to pre-load all the ids
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products
+    .map((p) => p.id)
+    .map((id) => ({ params: { id: id } }));
+
+  return {
+    paths: ids,
+    fallback: false,
+  };
+}
+
 const ProductDetailPage = ({ product }) => {
-  console.log(product);
+  if (!product) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="product-wrapper">
